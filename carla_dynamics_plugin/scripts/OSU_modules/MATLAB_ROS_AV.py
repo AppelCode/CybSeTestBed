6372616fbc84050b1d414da12ac721916233a960
matt <appel.60@osu.edu>
2019-08-03 09:06:10 -0400

    python module creation

--- carla_dynamics_plugin/scripts/OSU_modules/MATLAB_ROS_AV.py
+++ carla_dynamics_plugin/scripts/OSU_modules/MATLAB_ROS_AV.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
-
-# ==============================================================================
-# -- Setup ---------------------------------------------------------------------
-# ==============================================================================
-import glob
-import os
-import sys
-import time 
-import signal
-
-#uncomment and adjust if carla is not in your pyhton path
-#try:
-#    sys.path.append(glob.glob('/home/matt/Documents/CybSe/CARLA/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
-#        sys.version_info.major,
-#        sys.version_info.minor,
-#        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
-#except IndexError:
-#    pass
-
 import rospy
 from ackermann_msgs.msg import AckermannDrive
 from geometry_msgs.msg import Twist
@@ -44,8 +24,6 @@ from sympy import *
 import multiprocessing
 from threading import Lock
 
-import MATLAB_ROS_AV.AV as av
-
 ######## TESTING ########
 way_points = np.loadtxt('refPoses.dat')
 way_points = np.asarray(way_points)
@@ -57,7 +35,7 @@ way_points_act = np.asarray(way_points_act)
 # -- Robotics Stuff ------------------------------------------------------------
 # ==============================================================================
 
-class RoboticHelper():
+class _RoboticHelper():
     
     #return numpy representation of SE3 transform matrix
     #
@@ -168,11 +146,7 @@ class RoboticHelper():
 
         return omg, theta
 
-# ==============================================================================
-# -- Vehicle Contorl Process ---------------------------------------------------
-# ==============================================================================
-
-class VehicleVelocityControl():
+class _VehicleVelocityControl():
     #TODO: add proper intialization for orientation 
     def __init__(self,id):
         self.car_id = id 
@@ -192,12 +166,15 @@ class VehicleVelocityControl():
         numjobs = 1 
         jobs = []
         for i in range(numjobs):
-            p = multiprocessing.Process(target=self.vehicle_control)
+            p = multiprocessing.Process(target=self._vehicle_control)
             jobs.append(p)
             p.start()
 
     #process used to move vehicle in carla
-    def vehicle_control(self):
+    #TODO: handle smooth exit
+    #TODO: update inputs for host and port
+    
+    def _vehicle_control(self):
 
         def exit_child_process(signum, frame):
             signal.signal(signal.SIGINT, self.original_sigint)
@@ -249,17 +226,11 @@ class VehicleVelocityControl():
         self.velocity_set_args['x_angular_vel'] = np.asscalar(twist[3])
         self.velocity_set_args['y_angular_vel'] = np.asscalar(twist[4])
         self.velocity_set_args['z_angular_vel'] = np.asscalar(twist[5])
-        
-# ==============================================================================
-# -- Autonomous Vehicle --------------------------------------------------------
-# ==============================================================================
-#using a gloabl variable for the vehicle will not create link and call descunstrutor 
-#at exit
 
 class AV(RoamingAgent):
     # Robotic Helper is used for Screw Theory Mathmatics, northwestern modern robotics 
     # source cited in imports
-    rh = RoboticHelper()
+    rh = _RoboticHelper()
 
     # World orientation is left hand cooridinate 
     world_orientation = np.array([[1, 0, 0],
@@ -281,7 +252,7 @@ class AV(RoamingAgent):
         self._twist = np.zeros((6,1))
         
         #vehicle control process class
-        self.vc = VehicleVelocityControl(self._vehicle.id)
+        self.vc = _VehicleVelocityControl(self._vehicle.id)
 
         #waypoint generator
         self._local_planner = LocalPlanner(self._vehicle)
@@ -366,6 +337,7 @@ class AV(RoamingAgent):
         self.carla_set_publisher.publish(msg)
 
     #desired angle to next point
+    #TODO: unused
     def _update_path_angle(self):
 
         next_waypoint = self._current_waypoint.next(1)[0]
@@ -589,120 +561,3 @@ class AV(RoamingAgent):
 
     #def __del__(self):
         #print("deleted")
-
-# ==============================================================================
-# -- main loop -----------------------------------------------------------------
-# ==============================================================================
-def loop(car):
-
-    #matlab vehicle scenerio test intial conditions
-    #works on Town01
-    transform = carla.Transform()
-    transform.location.x = 302.77
-    transform.location.y = 57.5001
-    transform.location.z = 0
-    transform.rotation.pitch = 0
-    transform.rotation.yaw = -0.0958
-    transform.rotation.roll = 0
-
-    car._vehicle.set_transform(transform)
-
-    #remove tire friction to allow for smooth angular velocity control
-    vehicle_dynamics = car._vehicle.get_physics_control()
-    wheel_phyics = vehicle_dynamics.wheels
-    for i in range(4):
-        wheel_phyics[i].tire_friction = 0.0
-
-    vehicle_dynamics.wheels = wheel_phyics
-    car._vehicle.apply_physics_control(vehicle_dynamics)
-    while True:
-
-        #path planning updates, map based AV 
-        car.run_step()   
-        time.sleep(0.001)
-
-def exit_handler(signum, frame):
-    print('\ncleaning up and closing carla plugin')
-    sys.exit(1)
-
-def test_velocity_process(car):
-        #path planning updates, map based AV 
-        car.run_step()   
-        
-        #max_speed = car._set_speed
-        #print("current location: (%s, %s, %s)"%(car._current_transform_c.location.x, \
-        #    car._current_transform_c.location.y,car._current_transform_c.location.z))
-        #print("next location   : (%s, %s, %s)"%(car._next_transform_c.location.x, \
-        #    car._next_transform_c.location.y,car._next_transform_c.location.z))
-
-        #current vehicle rotation matrix
-        Rc = car._current_rotation_r
-
-        #find velocity direction
-        velocity_dir = Rc[:,0]  
-        velocity_dir = velocity_dir.reshape(1,3)     
-        velocity_dir = np.transpose(velocity_dir)
-
-        #update velocity direction and magnitude 
-        #to be handled by vehicle_control Process
-        car.vc.velocity_set_args['direction_x'] = velocity_dir[0,0]
-        car.vc.velocity_set_args['direction_y'] = velocity_dir[1,0]
-        car.vc.velocity_set_args['direction_z'] = velocity_dir[2,0]
-
-        car.vc.velocity_set_args['x_angular_vel'] = temp[0]
-        car.vc.velocity_set_args['y_angular_vel'] = temp[1]
-        car.vc.velocity_set_args['z_angular_vel'] = temp[2]
-
-        car.vc.velocity_set_args['magnitude'] = input("Enter new Speed [km/hr]: ") 
-
-        #path planning algorithm and scalling function
-        #solve for coefficents
-        # #s(t) = a0 + a1t + a2t +a3t : s(0) = 0 s(car._sampling_time) = 1
-
-        #definiton of path using scaling funciton: X(s) = Tcexp(log(Tc^(-1)Tn)s)
-        #run the path
-        time.sleep(0.1)
-
-if __name__ == '__main__':
-    #create argument parser
-    argparser = argparse.ArgumentParser(description=__doc__)
-    argparser.add_argument(
-        '--rolename',
-        metavar = 'rolename',
-        default = 'hero',
-        help = 'enter id of vehicle to control'
-    )
-    argparser.add_argument(
-        '--host',
-        metavar='H',
-        default='192.168.1.36',
-        help='IP of the host server (default: 127.0.0.1)'
-    )
-    argparser.add_argument(
-        '-p', '--port',
-        metavar='P',
-        default=2000,
-        type=int,
-        help='TCP port to listen to (default: 2000)'
-    )
-    args = argparser.parse_args()
-
-    #connect to the client
-    client = carla.Client(args.host,args.port)
-    try:
-        #world = client.load_world('Town01')
-        world = client.get_world()          #grab the world
-        actors = world.get_actors()         #grab all actors
-
-        for temp in actors:
-            if temp.attributes.has_key('role_name'):
-                role = temp.attributes['role_name']
-                if role == "ego_vehicle":
-                    actor = temp
-
-        car = av(world,actor)
-        signal.signal(signal.SIGINT, exit_handler)
-        loop(car)
-
-    except rospy.ROSInterruptException:
-        pass
